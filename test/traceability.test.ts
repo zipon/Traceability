9d2d7e88843cafa6e9714dbecf80b493cebc4add
zipon <lars.andersson@polarismedia.no>
2026-01-28 12:28:42 +0100

    Initial commit

---
+++ test/traceability.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Traceability from '../lib/traceability-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/traceability-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Traceability.TraceabilityStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
