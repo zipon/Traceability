7b5c56a82b16616acc5ab1aa55e18d51f31bf188
zipon <lars.andersson@polarismedia.no>
2026-01-28 13:42:04 +0100

    Adding config file and the plumbing of the tracing of the deployment info with default Manually deployed. Added stack level Tagging.

---
+++ lib/config.ts
@@ -0,0 +1,9 @@
+export const Config = {
+  COST_TAG_PROJECT: 'AI-Dataplatform',
+  COST_TAG_MODULE: 'ai-dataplatform-frontend',
+  COST_TAG_SUBMODULE: 'traceability-submodule',
+  COST_TAG_SUBMODULE_STACK_TWO: 'network-submodule',
+  COST_TAG_RESOURCE: 'present-third-party-data',
+  MANAGED_BY: 'Lars Andersson',
+  BACKUP_REQUIRED: 'no',
+};
--- lib/traceability-stack.ts
+++ lib/traceability-stack.ts
@@ -1,16 +1,38 @@
 import * as cdk from 'aws-cdk-lib/core';
+import { Stack, StackProps, CfnOutput, Tags } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Config } from './config';
 
 export class TraceabilityStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Tags
+    Tags.of(this).add('costTag-project', Config.COST_TAG_PROJECT);
+    Tags.of(this).add('costTag-module', Config.COST_TAG_MODULE);
+    Tags.of(this).add('costTag-submodule', Config.COST_TAG_MODULE);
+    Tags.of(this).add('costTag-resource', Config.COST_TAG_RESOURCE);
+    Tags.of(this).add('managedBy', Config.MANAGED_BY);
+    Tags.of(this).add('backup-required', Config.BACKUP_REQUIRED);
+
+    // Deployment info from context values (optional)
+    const gitCommit = this.node.tryGetContext('gitCommit') as string | undefined;
+    const repoName = this.node.tryGetContext('repoName') as string | undefined;
+    const branch = this.node.tryGetContext('branchName') as string | undefined;
+
+    // Builds the deployments info and if it is not deployed from GitHub action it will be Manually deployed
+    const deployInfo = (gitCommit && repoName && branch)
+     ? `${repoName} (${branch}) @ ${gitCommit.substring(0, 7)}`
+     : 'Manually deployed';
+
+    const rawBranch: string = branch ?? "dev-manual";
+    const branchName: string = rawBranch === "main" || rawBranch === "master" ? "prod" : rawBranch;
+
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TraceabilityQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CfnOutput(this, 'DeploymentInfo', {
+        value: deployInfo,
+        description: 'Source of this deployment (repo, branch, commit) or manual deploy marker'
+    });
   }
 }
