32d2c5e7381956de5731a26b2619538dbadc458a
zipon <lars.andersson@polarismedia.no>
2026-01-30 09:26:32 +0100

    Added another stack to make the example more clear. Some smal updates to the config.

--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,9 @@
 export const Config = {
-  COST_TAG_PROJECT: 'AI-Dataplatform',
-  COST_TAG_MODULE: 'ai-dataplatform-frontend',
+  COST_TAG_PROJECT: 'Traceability',
+  COST_TAG_MODULE: 'traceability-demo',
   COST_TAG_SUBMODULE: 'traceability-submodule',
-  COST_TAG_SUBMODULE_STACK_TWO: 'network-submodule',
-  COST_TAG_RESOURCE: 'present-third-party-data',
+  COST_TAG_SUBMODULE_STACK_TWO: 'pink-fluffy-unicorns-submodule',
+  COST_TAG_RESOURCE: 'present-traceability',
   MANAGED_BY: 'Lars Andersson',
   BACKUP_REQUIRED: 'no',
 };
